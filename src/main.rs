5caf90f973752fe399b88d273dbf9517a70dd9d2
Carlos Navajas <c.navajas4@gmail.com>
2025-08-24 22:03:11 +0200

    init: exploration of vec and slice implementation

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+struct MyVec<T> {
+    data: Box<[T]>
+}
+
+impl<T> MyVec<T> {
+    fn new(v: Vec<T>) -> Self {
+        Self { data: v.into_boxed_slice() }
+    }
+
+    fn as_slice(&self) -> &[T] {
+        &self.data
+    }
+
+    fn as_slice_mut(&mut self) -> &mut [T] {
+        &mut self.data
+    }
+}
+
+fn main() {
+    let mv = MyVec::new(vec![10, 20, 30]);
+
+    let mut slice = mv.as_slice();
+    println!("slice: {:?}", slice);
+    println!("len: {}", slice.len());
+    println!("first: {:?}", slice.get(0));
+
+    slice[0] += 1
+}
